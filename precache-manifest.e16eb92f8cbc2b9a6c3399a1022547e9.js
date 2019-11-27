c2ce0ea6c3612a9a30a4071f80e8e4eda0d5a4a6
gselfzero <gselfzero@users.noreply.github.com>
2019-11-27 23:11:51 +0300

    a
    a

---
+++ precache-manifest.e16eb92f8cbc2b9a6c3399a1022547e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc9b0bf38329d4699b01",
+    "url": "/coinmi/static/css/main.a47842da.chunk.css"
+  },
+  {
+    "revision": "dc9b0bf38329d4699b01",
+    "url": "/coinmi/static/js/main.4d84427c.chunk.js"
+  },
+  {
+    "revision": "c2f409ee4f432e8120fe",
+    "url": "/coinmi/static/js/runtime~main.ac2f6457.js"
+  },
+  {
+    "revision": "678fe4d35f32ae3ab419",
+    "url": "/coinmi/static/css/2.7fe1ace6.chunk.css"
+  },
+  {
+    "revision": "678fe4d35f32ae3ab419",
+    "url": "/coinmi/static/js/2.b1751dfd.chunk.js"
+  },
+  {
+    "revision": "422a1cffb3592ad79374c925b6282f0c",
+    "url": "/coinmi/static/media/click.422a1cff.png"
+  },
+  {
+    "revision": "caf671e94fd2a15ed8c3ea9df67062eb",
+    "url": "/coinmi/index.html"
+  }
+];
